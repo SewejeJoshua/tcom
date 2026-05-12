7de8135add4d27be1c23d05bd63b5e1d08bbe027
CognitoJosh <joshuaseweje2@gmail.com>
2026-05-12 15:51:34 +0100

    updated ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "@tanstack/react-router";
-import { router } from "./router";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 import "./index.css";
+
+const router = createRouter({
+  routeTree,
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
