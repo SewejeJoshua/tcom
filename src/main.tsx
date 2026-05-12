48c5d369abc8666c9b8d65f6e6c3081ef8161654
CognitoJosh <joshuaseweje2@gmail.com>
2026-05-12 14:59:53 +0100

    Fix router entry, remove App.tsx dependency

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
 import "./index.css";
+
+const router = createRouter({
+  routeTree,
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
